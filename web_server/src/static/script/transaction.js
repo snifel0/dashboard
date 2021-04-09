f31502ed2e96ca38711bc7690d90cfcd8e1533a7
Jinwoo Kim <snifel0@gmail.com>
2021-04-08 23:31:37 -0400

    add transaction feature

---
+++ web_server/src/static/script/transaction.js
@@ -0,0 +1,63 @@
+function changeState(params) {
+    window.history.replaceState({}, '', `${location.pathname}?${params}`);
+    location.reload();
+}
+function Position(ticker, shares) {
+    this.ticker = ticker;
+    this.shares = shares;
+}
+function getPositions() {
+    const urlParams = new URLSearchParams(window.location.search);
+    var cash = 0;
+    var positions = [];
+    var count = 0;
+    for (const key of urlParams.keys()) {
+        if (key == "$$CASH$$") {
+            cash = urlParams.get(key);
+        } else {
+            positions[count] = new Position(key, urlParams.get(key));
+            count++;
+        }
+    }
+    return [positions, cash];
+}
+function getNewQueryURI(positions, cash) {
+    var new_urlParams = new URLSearchParams();
+    new_urlParams
+    for (var i = 0; i < positions.length; i++) {
+        new_urlParams.append(positions[i].ticker, positions[i].shares.toString());
+    }
+    new_urlParams.append("$$CASH$$", cash.toString());
+    return new_urlParams.toString();
+}
+function applyTransaction() {
+    var myDiv = document.getElementById("debug");
+    var [old_positions, old_cash] = getPositions();
+    var ticker = document.getElementById("ticker").value;
+    var price_per_share = document.getElementById("price-per-share").value;
+    if (document.getElementById("buy").checked) {
+        var shares = document.getElementById("shares").value;
+    } else if (document.getElementById("sell").checked) {
+        var shares = - document.getElementById("shares").value;
+    } else {
+        var transaction = "No transaction";
+    }
+    cost = shares * price_per_share;
+    var new_cash = old_cash - cost;
+    var old_shares = 0;
+    var new_positions = [];
+    var new_i = 0;
+    for (let i = 0; i < old_positions.length; i++) {
+        if (old_positions[i].ticker == ticker) {
+            old_shares += old_positions[i].shares;
+        } else {
+            new_positions[new_i] = old_positions[i];
+            new_i++;
+        }
+    }
+    var new_shares = parseInt(old_shares) + parseInt(shares);
+    if (new_shares != 0) {
+        new_positions[new_i] = new Position(ticker, new_shares);
+    }
+    changeState(getNewQueryURI(new_positions, new_cash));
+}
